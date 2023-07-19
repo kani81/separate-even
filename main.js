b585c92adb29fa96d5c854dd2e0b145bce2b476f
kani81 <kaniarasiduraisamy.com>
2023-07-19 15:13:57 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,9 @@
+var arr=[11,10,70,75,59,2,4];
+document.write("arr=[11,10,70,75,59,2,4]"+"<br>"+"<br>");
+for(let i=0; i<arr.length; i++){
+		if(arr[i]%2==0){
+			document.write(arr[i]);
+			document.write("&nbsp ");
+			
+		}
+}
